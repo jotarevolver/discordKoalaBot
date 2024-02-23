098bdf8601b5764fb9c4dbb965f6d98045e276e7
jotarevolver <116527059+jotarevolver@users.noreply.github.com>
2024-02-22 23:48:18 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const { Client, GatewayIntentBits } = require('discord.js');
+const keep_alive = require('./keep_alive.js')
+
+const client = new Client({ intents: 3276799 });
+
+client.on('ready', () => {
+  console.log(`Bot conectado como ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  // Verifica que el mensaje provenga de un servidor (guild) y no sea del bot
+  if (message.guild && !message.author.bot) {
+    // Expresión regular para encontrar enlaces de x.com
+    const xLinkRegex = /https:\/\/x\.com\/.*/;
+
+    // Busca enlaces de x.com en el mensaje
+    const xLinks = message.content.match(xLinkRegex);
+
+    // Si se encuentran enlaces, modifícalos y responde
+    if (xLinks && xLinks.length > 0) {
+      const modifiedLinks = xLinks.map(link => link.replace('https://x.com/', 'https://fxtwitter.com/'));
+
+      // Menciona al autor original y envía el enlace modificado
+      await message.channel.send(`${message.author.toString()}, envió:\n${modifiedLinks.join('\n')}`);
+
+      // Borra el mensaje original
+      await message.delete();
+    }
+  }
+});
+
+// Asegúrate de mantener tu token seguro
+client.login(process.env.TOKEN);
