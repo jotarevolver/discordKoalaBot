607673dd885818a4a149b75ce0203a3fe89c1adf
jotarevolver <junmcogno@gmail.com>
2024-09-08 18:43:40 -0300

    fix getFinalScript

--- eventos/getFinalUrl.js
+++ eventos/getFinalUrl.js
@@ -1,23 +1,19 @@
-import fetch from 'node-fetch';
-
 async function getFinalUrl(shortUrl) {
-    try{
-        const response = await fetch (shortUrl, {
+    const fetch = (await import('node-fetch')).default;
+
+    try {
+        const response = await fetch(shortUrl, {
             method: 'HEAD',
             redirect: 'manual'
         });
-    
+
         if (response.status >= 300 && response.status < 400) {
-            // La URL de redirección estará en la cabecera 'location'.
             const finalUrl = response.headers.get('location');
             return finalUrl;
         } else {
             return "No redirection found";
         }
-    } 
-    catch (error) {
+    } catch (error) {
         console.error('Error:', error);
     }
 }
-    
-
