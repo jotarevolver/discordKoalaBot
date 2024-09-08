b98e3d0332c4b4d3fd6f2e257c290b9194d6a4ea
jotarevolver <junmcogno@gmail.com>
2024-09-08 18:48:59 -0300

    the really important fix of index

--- index.js
+++ index.js
@@ -154,6 +154,26 @@ client.on('messageCreate', async (mensaje) => {
 
 });
 
+async function getFinalUrl(shortUrl) {
+  const fetch = (await import('node-fetch')).default;
+
+  try {
+      const response = await fetch(shortUrl, {
+          method: 'HEAD',
+          redirect: 'manual'
+      });
+
+      if (response.status >= 300 && response.status < 400) {
+          const finalUrl = response.headers.get('location');
+          return finalUrl;
+      } else {
+          return "No redirection found";
+      }
+  } catch (error) {
+      console.error('Error:', error);
+  }
+}
+
 let handlers = ['eventos', 'comandos'];
 handlers.forEach(handler => {
     require(`./handlers/${handler}`)(client);
